0e9eec22072d96fdb5720651a727b4a7a7130320
SanjayDutta <sanjaydutta976.com>
2024-04-23 11:08:24 +0530

    router and controller configured

--- src/app.js
+++ src/app.js
@@ -17,11 +17,9 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 // routes import
-import userRouter from './routes/user.route.js'
+import userRouter from "./routes/user.route.js";
 
 //routes declaration
-app.use("/api/v1/users",userRouter )
-
-
+app.use("/api/v1/users", userRouter);
 
 export { app };
